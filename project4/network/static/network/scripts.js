e769c043b40576484a4c3995bec3c36ba569258d
Luiz Mainart <luizdmmainart@gmail.com>
2021-10-11 12:23:59 -0300

    "Edit Post" Assignment part 3

--- project4/network/static/network/scripts.js
+++ project4/network/static/network/scripts.js
@@ -2,11 +2,41 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".editPost").forEach((button) =>
     button.addEventListener("click", function () {
       postDiv = button.parentElement.parentElement.parentElement;
-      postDiv.innerHTML = editPostForm(postDiv.id);
+      postDivClone = postDiv.cloneNode(true);
+      postDiv.innerHTML = '';
+      postDiv.appendChild(editPostForm(postDivClone));
+      postDiv.childNodes[0].addEventListener("submit", submitEditPostForm(postDivClone));
     })
   );
 });
 
-function editPostForm(id) {
-  return '<form><textarea name="content" cols="40" rows="10" class="form-control" id="postContent" required=""></textarea><input type="submit" value="Save" class="btn btn-primary"></form>';
+
+function submitEditPostForm(postDiv) {
+  fetch(`/post/${postDiv.id}/edit`, {
+    method: 'POST',
+    body: JSON.stringify({
+      content: document.querySelector(`#content${postDiv.id}`).value
+    })
+  })
+}
+
+
+function editPostForm(postDiv) {
+  form = document.createElement("form");
+  textarea = document.createElement("textarea");
+  textarea.setAttribute("name", "content");
+  textarea.setAttribute("id", `content${postDiv.id}`);
+  textarea.setAttribute("cols", "40");
+  textarea.setAttribute("rows", "10");
+  textarea.setAttribute("class", "form-control");
+  textarea.setAttribute("id", "postContent");
+  textarea.setAttribute("required", "true");
+  textarea.innerHTML = postDiv.childNodes[1].innerText;
+  submitButton = document.createElement("input");
+  submitButton.setAttribute("type", "submit");
+  submitButton.setAttribute("value", "Save");
+  submitButton.setAttribute("class", "btn btn-primary");
+  form.appendChild(textarea);
+  form.appendChild(submitButton);
+  return form;
 }
