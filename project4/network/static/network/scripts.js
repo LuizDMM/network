ee57efb1284944509daddb77b88c63365dfa0953
Luiz Mainart <luizdmmainart@gmail.com>
2021-10-15 19:42:49 -0300

    "Edit Post" Assignment part 4 - Fetch bug fixed

--- project4/network/static/network/scripts.js
+++ project4/network/static/network/scripts.js
@@ -15,43 +15,59 @@ function showEditPostForm(postDiv) {
     .then((response) => {
       let div = document.querySelector(`#${postDiv.id}`);
       let form = document.createElement("form");
-      form.setAttribute("action", `/post/${postDiv.id}/edit`);
-      form.setAttribute("method", "post");
+      /* form.setAttribute("action", `/post/${postDiv.id}/edit`);
+      form.setAttribute("method", "post"); */
       let id = postDiv.id.split("-")[1];
       form.setAttribute("id", `editPost-${id}`);
+      form.setAttribute("onsubmit", `submitEditPostForm(${id})`);
       form.innerHTML = response;
       div.innerHTML = "";
       div.appendChild(form);
-      form.onsubmit = () => {
-        return submitEditPostForm(postDiv, div);
-      };
     });
 }
 
-function submitEditPostForm(postDiv, div) {
-  let id = postDiv.id.split("-")[1];
+// Function from "https://docs.djangoproject.com/en/3.2/ref/csrf/"
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
+const csrftoken = getCookie("csrftoken");
+
+function submitEditPostForm(id) {
   let form = document.querySelector(`#editPost-${id}`);
-  fetch(`/post/${postDiv.id}/edit`, {
+  let div = document.querySelector(`#post-${id}`);
+
+  fetch(`/post/post-${id}/edit`, {
     method: "POST",
     body: JSON.stringify({
       csrfmiddlewaretoken: form.children[0].value,
       content: form.children[1].value,
       credentials: "same-origin",
     }),
-  })
-    .then((response) => response.json())
-    .then((response) => {
-      if (response.status == 400) {
-        alert(`Error: ${response.error}`);
-      } else {
-        fetch(`/post/${postDiv.id}`)
-          .then((response) => response.text())
-          .then((response) => {
-            div.innerHTML = response;
-          });
-      }
-    });
+    headers: { "X-CSRFToken": csrftoken },
+  }).then((response) => {
+    if (response.status == 400) {
+      alert(`Error: ${response.error}`);
+    } else {
+      fetch(`/post/post-${id}`)
+        .then((response) => response.text())
+        .then((response) => {
+          div.innerHTML = response;
+        });
+    }
+  });
 
   return false;
 }
-
